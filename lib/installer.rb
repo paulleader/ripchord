7aac794470272cf0406d27bfb830097f18daa422
Paul Leader <paul@paulleader.co.uk>
2015-02-28 18:47:45 +0000

    Fix logging in Installer

--- lib/installer.rb
+++ lib/installer.rb
@@ -1,7 +1,7 @@
 class Installer < ProcessThread
 
   def _process(job)
-    @log.debug "Installer: Installing #{job.title} to #{job.destination_directory}"
+    @log.info "Installer: Installing #{job.title} to #{job.destination_directory}"
     FileUtils.move(@working_area.converting_file(job.title), job.destination_directory)
     job.state = nil
   end
